28a99da36bcf07d544ac126ae3b54ddda2a84287
abhijittapadar <tapadarabhijit@gmail.com>
2025-04-01 12:53:01 +0530

    First Commit

---
+++ sg.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+    region = "us-east-1"
+  
+}
+resource "aws_security_group" "MySG" {
+  
+}
+resource "aws_security_group_rule" "ingress" {
+  security_group_id = aws_security_group.MySG.id
+  type              = "ingress"
+  from_port         = 80          # Example port (e.g., HTTP)
+  to_port           = 80          # Example port (e.g., HTTP)
+  protocol          = "tcp"
+  cidr_blocks       = ["10.0.0.0/24"]  # Source IP range
+}
