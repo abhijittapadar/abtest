727f0d8500d272a771ebde05f58808abcec21d7e
abhijittapadar <tapadarabhijit@gmail.com>
2025-04-01 13:09:44 +0530

    Some Change

--- sg.tf
+++ sg.tf
@@ -6,15 +6,15 @@ resource "aws_security_group" "MySG2" {
   
 }
 resource "aws_security_group_rule" "demo" {
-  security_group_id = aws_security_group.MySG.id
+  security_group_id = aws_security_group.MySG2.id
   type              = "ingress"
   from_port         = 80          # Example port (e.g., HTTP)
   to_port           = 80          # Example port (e.g., HTTP)
   protocol          = "tcp"
   cidr_blocks       = ["10.0.0.0/24"]  # Source IP range
 }
-resource "aws_security_group_rule" "ingress" {
-  security_group_id = aws_security_group.MySG.id
+resource "aws_security_group_rule" "demo1" {
+  security_group_id = aws_security_group.MySG2.id
   type              = "ingress"
   from_port         = 8080          # Example port (e.g., HTTP)
   to_port           = 8080          # Example port (e.g., HTTP)
