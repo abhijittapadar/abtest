16d3b4c4aaaa39ed288611d32da3e019d5d4bdf4
Abhijit Tapadar <126352509+abhijittapadar@users.noreply.github.com>
2025-04-01 12:57:34 +0530

    Update sg.tf

--- sg.tf
+++ sg.tf
@@ -13,3 +13,11 @@ resource "aws_security_group_rule" "ingress" {
   protocol          = "tcp"
   cidr_blocks       = ["10.0.0.0/24"]  # Source IP range
 }
+resource "aws_security_group_rule" "ingress" {
+  security_group_id = aws_security_group.MySG.id
+  type              = "ingress"
+  from_port         = 8080          # Example port (e.g., HTTP)
+  to_port           = 8080          # Example port (e.g., HTTP)
+  protocol          = "tcp"
+  cidr_blocks       = ["10.0.0.0/24"]  # Source IP range
+}
